113582c6443665f2a3d5893e60360d9553f09a91
root <root@srv1179273.hstgr.cloud>
2026-04-21 22:50:32 +0000

    Add /repair-services to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -28,5 +28,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'monthly',
       priority: 0.9,
     },
+    {
+      url: `${baseUrl}/repair-services`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.9,
+    },
   ];
 } 
